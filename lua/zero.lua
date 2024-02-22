e2400f91f6f11ff17850f1c4057933cf02c2242d
beto-bit <76757367+beto-bit@users.noreply.github.com>
2024-02-22 17:30:36 -0600

    This really works

--- lua/zero.lua
+++ lua/zero.lua
@@ -14,12 +14,28 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup {
+    -- Select first item
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
+
+    -- Completion menu borders
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+
     mapping = cmp.mapping.preset.insert {
         -- `Enter` to confirm selection
         ['<CR>'] = cmp.mapping.confirm { select = false },
 
         -- Ctrl+Space like VSCode
         ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Super Tab
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
 
         -- Navigate between snippet placeholder
         ['<C-f>'] = cmp_action.luasnip_jump_forward(),
