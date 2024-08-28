0ab096032fb270d3d91d96779121ca7317c6b7c6
beto-bit <76757367+beto-bit@users.noreply.github.com>
2024-08-28 16:04:07 -0600

    Added lualine and better indents

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        main = 'ibl',
+        opts = {
+            scope = {
+                enabled = false,
+                show_start = false,
+                show_end = false,
+            },
+        },
+    },
+}
