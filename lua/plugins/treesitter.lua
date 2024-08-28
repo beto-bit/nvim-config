abae3ea163364174327b987315f8f8f2c63c3483
beto-bit <76757367+beto-bit@users.noreply.github.com>
2024-08-27 23:31:11 -0600

    Added Treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,43 @@
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                highlight = {
+                    enable = true
+                },
+                sync_install = true,
+                ensure_installed = {
+                    'lua',
+
+                    -- Web
+                    'css',
+                    'html',
+                    'json',
+                    'javascript',
+                    'typescript',
+
+                    -- Low level shit
+                    'c',
+                    'cpp',
+                    'cmake',
+                    'rust',
+                    'make',
+                    'zig',
+
+                    -- I hate this shit
+                    'java',
+
+                    -- Other
+                    'bash',
+                    'diff',
+                    'gitignore',
+                    'gitattributes',
+                    'python',
+                    'toml'
+                }
+            }
+        end
+    },
+}
