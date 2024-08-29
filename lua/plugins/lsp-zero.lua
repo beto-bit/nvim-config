6ad4aafe0af1eb2304966187ff4237ea463a87e0
beto-bit <76757367+beto-bit@users.noreply.github.com>
2024-08-29 07:38:40 -0600

    Basic, but working LSP config

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,10 +1,20 @@
+local lsp_attach = function(client, bufnr)
+    local opts = {buffer = bufnr}
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+end
+
+
 return {
-    {
-        'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'
-    },
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
     {
         'williamboman/mason.nvim',
         lazy = false,
@@ -12,5 +22,59 @@ return {
         opts = {
             ui = { border = 'rounded' },
         },
+    },
+    { 'williamboman/mason-lspconfig.nvim' },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v4.x',
+        config = false,
+    },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        config = function()
+            local cmp = require('cmp')
+
+            cmp.setup {
+                sources = {
+                    { name = 'nvim_lsp' },
+                },
+                mapping = cmp.mapping.preset.insert {
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                },
+                snippet = {
+                    expand = function(args)
+                        vim.snippet.expand(args.body)
+                    end
+                },
+            }
+        end
+    },
+    { 'L3MON4D3/LuaSnip' },
+    {
+        'neovim/nvim-lspconfig',
+        cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+        event = { 'BufReadPre', 'BufNewFile' },
+        config = function()
+            lsp_zero = require('lsp-zero')
+
+            lsp_zero.extend_lspconfig {
+                sign_text = true,
+                lsp_attach = lsp_attach,
+                capabilities = require('cmp_nvim_lsp').default_capabilities(),
+            }
+
+            require('mason-lspconfig').setup {
+                ensure_installed = {},
+                handlers = {
+                    function(server_name)
+                        require('lspconfig')[server_name].setup{}
+                    end
+                }
+            }
+        end
     },
 }
