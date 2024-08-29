ce5f61a4167cbad96fad9d9a0e60fea733e5437c
beto-bit <76757367+beto-bit@users.noreply.github.com>
2024-08-28 23:24:47 -0600

    Added telescope keybindings

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,15 +2,9 @@ return {
     {
         'folke/which-key.nvim',
         event = 'VeryLazy',
-        opts = {},
-        -- TODO: Make this work
-        -- keys = {
-        --     '<leader>?',
-        --     function()
-        --         require('which-key').show({ global = false })
-        --     end,
-        --     desc = 'Buffer Local Keymaps (which-key)',
-        -- },
+        opts = {
+            preset = 'modern',
+        },
     },
     { 'echasnovski/mini.icons', version = false },
 }
