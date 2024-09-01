7007cc81ba708d17d49e3d8e69973a2766e0d5c2
beto-bit <76757367+beto-bit@users.noreply.github.com>
2024-09-01 16:46:32 -0600

    Added a bunch of themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,4 +1,9 @@
 return {
+    { 'navarasu/onedark.nvim', opts = { style = 'darker', } },
+    { 'folke/tokyonight.nvim' },
+    { 'rebelot/kanagawa.nvim' },
+    { '0xstepit/flow.nvim' },
+    { 'EdenEast/nightfox.nvim' },
     {
         'Shatur/neovim-ayu',
         lazy = false,
@@ -6,5 +11,5 @@ return {
         config = function()
             require('ayu').colorscheme()
         end
-    }
+    },
 }
