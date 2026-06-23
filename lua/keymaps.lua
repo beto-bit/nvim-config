dac3fec8438d7b8af5e754e2896c81c3e2897585
beto-bit <76757367+beto-bit@users.noreply.github.com>
2026-06-23 07:57:15 -0600

    Added `yazi.nvim` to the project

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,12 @@
 -- Telescope
 vim.keymap.set('n', '<leader>f', '<cmd>Telescope<cr>', { desc = 'Telescope' })
+
+-- Yazi
+vim.keymap.set(
+    'n',
+    '<leader>s',
+    function()
+        require('yazi').yazi()
+    end,
+    { desc = 'Yazi' }
+)
